65810f07ca1e7459e780ed7390739fa7e579fe9a
sophia39 <sfidante.kanna@gmail.com>
2018-01-18 15:04:45 +0900

    StepAsideUnityChan レッスン６の課題
    不要になったオブジェクトを破棄するコーディングをおこないました。ちなみに発展課題はやっていません。

---
+++ Assets/CubeBGM.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBGM : MonoBehaviour {
+
+    public AudioClip se;
+    public int SEon;
+    private int speed = 40;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(this.SEon >= 0)
+        {
+            GetComponent<AudioSource>().PlayOneShot(se);
+            this.SEon -= this.speed;
+        }
+       
+    }
+   
+    
+
+    void OnCollisionEnter(Collision2D collision)
+    {
+        this.SEon = 180;
+
+    }
+
+
+}
